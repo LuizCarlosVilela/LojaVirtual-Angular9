9d70202555f575bd8418bd5be5ec29045023a2bb
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-12 17:58:03 -0300

    Completando o update dos produtos, ligando o frontend com o backend

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Producto Atualizado com sucesso!')
+
+      this.router.navigate(['/products'])
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
