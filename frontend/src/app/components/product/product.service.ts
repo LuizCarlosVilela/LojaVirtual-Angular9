bac00bd038c5829ebbbbc728347496676c625ffe
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-12 18:07:49 -0300

    Fazendo método de delete no service

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -43,4 +43,11 @@ export class ProductService {
     
     return this.http.put<Product>(url, product)
   }
+
+  delete(id: string): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`
+
+    return this.http.delete<Product>(url)
+  }
+
 }
